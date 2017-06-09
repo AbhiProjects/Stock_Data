d9d362f011aceda19ec550b512915c60e49f4f7b
Abhishek Chawla <AbhiProjects@users.noreply.github.com>
2017-06-09 10:20:42 +0530

    Heroku Time Change Revert

--- clock.py
+++ clock.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from main import main
 
 sched = BlockingScheduler()
-sched.add_job(main, 'cron', day_of_week='mon-fri', hour=10, minute=20, timezone='Asia/Kolkata')
+sched.add_job(main, 'cron', day_of_week='mon-fri', hour=9, minute=30, timezone='Asia/Kolkata')
 sched.start()
